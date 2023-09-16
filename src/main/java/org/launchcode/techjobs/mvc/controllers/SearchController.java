a8fb7a580a08836742cd80b0811996873d17d447
Greg <138933807+sinistergreg@users.noreply.github.com>
2023-09-16 13:43:16 -0600

    Assignment 3 - only 1 test failing

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -28,21 +28,21 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-    @PostMapping(value = "results")
-    public String displaySearchResults(Model model,
-                                       @RequestParam("searchType") String searchType,
-                                       @RequestParam("searchTerm") String searchTerm) {
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam  String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
-
-        if (searchTerm == null || searchTerm.isEmpty() || searchTerm.equalsIgnoreCase("all")) {
+        if (searchTerm.equals("all") || searchTerm.isEmpty()) {
             jobs = JobData.findAll();
+            model.addAttribute("title", "All Jobs");
         } else {
-            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+            jobs =  JobData.findByColumnAndValue(searchType,searchTerm);
         }
 
-        model.addAttribute("jobs", jobs);
         model.addAttribute("columns", columnChoices);
+        model.addAttribute("jobs",jobs);
 
         return "search";
     }
+
+
 }
